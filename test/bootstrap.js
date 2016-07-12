bb8aeacb4dd19ab09773dbadb1b26368ad649dcd
Patrick Hulce <patrick@solvvy.com>
2016-07-12 10:09:46 -0700

    fix(Model): validate passes tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -6,8 +6,9 @@ chai.should();
 chai.use(require('sinon-chai'));
 
 global.sandbox = () => sinon.sandbox.create();
+global.relativeRequire = file => require('../lib/' + file);
 global.defineTest = (file, func) => {
   describe(file, function () {
     func(require('../lib/' + file));
   });
-}
+};
