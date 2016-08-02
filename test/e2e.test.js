6b52bf2ce51e1beb45dcd4ef16a97dbcc81af6aa
Patrick Hulce <patrick.hulce@gmail.com>
2016-08-01 22:28:15 -0700

    fix: end-to-end validation

---
+++ test/e2e.test.js
@@ -0,0 +1,105 @@
+var _ = require('lodash');
+var klay = relativeRequire('klay.js');
+
+describe('klay', function () {
+  context('fixtures/document.js', function () {
+    var fixture = require('./fixtures/document.js');
+    var model;
+
+    beforeEach(function () {
+      model = fixture(klay());
+    });
+
+    afterEach(function () {
+      klay.reset();
+    });
+
+    it('should pass valid html document', function () {
+      var obj = {
+        id: '12345678-1234-1234-1234-123412341234',
+        parentId: '12345678-1234-1234-1234-123412341234',
+        type: 'html',
+        metadata: {title: 'my document', version: 'html5'},
+        source: {raw: '<html></html>', text: ''},
+        createdAt: '2016-07-27T04:51:22.820Z',
+        updatedAt: '2016-07-27T04:51:22.820Z',
+      };
+
+      var validated = model.validate(obj);
+      validated.should.have.property('conforms', true);
+      validated.should.have.property('value').eql(_.defaults({
+        createdAt: new Date('2016-07-27T04:51:22.820Z'),
+        updatedAt: new Date('2016-07-27T04:51:22.820Z'),
+      }, obj));
+    });
+
+    it('should pass valid json document', function () {
+      var obj = {
+        id: '12345678-1234-1234-1234-123412341234',
+        parentId: '12345678-1234-1234-1234-123412341234',
+        type: 'json',
+        metadata: {type: 'array', size: '120'},
+        source: {prop1: 'foobar', prop2: 'something'},
+        createdAt: '2016-07-27T04:51:22.820Z',
+        updatedAt: '2016-07-27T04:51:22.820Z',
+      };
+
+      var validated = model.validate(obj);
+      validated.should.have.property('conforms', true);
+      validated.should.have.property('value').eql(_.defaults({
+        metadata: {type: 'array', size: 120},
+        createdAt: new Date('2016-07-27T04:51:22.820Z'),
+        updatedAt: new Date('2016-07-27T04:51:22.820Z'),
+      }, obj));
+    });
+
+    it('should fail invalid html document', function () {
+      var obj = {
+        id: '12345678-1234-1234-1234-123412341234',
+        parentId: '12345678-1234-1234-1234-123412341234',
+        type: 'html',
+        metadata: {title: 'foo', version: 'v1', html5: true},
+        source: {raw: 'something'},
+        createdAt: '2016-07-27T04:51:22.820Z',
+        updatedAt: '2016-07-27T04:51:22.820Z',
+      };
+
+      var validated = model.validate(obj);
+      validated.should.have.property('conforms', false);
+      validated.should.have.property('errors').eql([
+        {message: 'unexpected properties: html5', path: 'metadata'},
+        {message: 'expected value to be defined', path: 'source.text'},
+      ]);
+
+      validated.should.have.property('value').eql(_.defaults({
+        source: {raw: 'something', text: undefined},
+        createdAt: new Date('2016-07-27T04:51:22.820Z'),
+        updatedAt: new Date('2016-07-27T04:51:22.820Z'),
+      }, obj));
+    });
+
+    it('should fail invalid json document', function () {
+      var obj = {
+        id: '12345678-1234-1234-1234-123412341234',
+        parentId: '12345678-1234-1234-1234-123412341234',
+        type: 'json',
+        metadata: {type: 'number', size: 'fifty'},
+        source: {a: 1, b: true, c: 'foo'},
+        createdAt: '2016-07-27T04:51:22.820Z',
+        updatedAt: '2016-07-27T04:51:22.820Z',
+      };
+
+      var validated = model.validate(obj);
+      validated.should.have.property('conforms', false);
+      validated.should.have.property('errors').eql([
+        {message: 'expected number to be one of ["object","array"]', path: 'metadata.type'},
+        {message: 'value must be an integer', path: 'metadata.size'},
+      ]);
+
+      validated.should.have.property('value').eql(_.defaults({
+        createdAt: new Date('2016-07-27T04:51:22.820Z'),
+        updatedAt: new Date('2016-07-27T04:51:22.820Z'),
+      }, obj));
+    });
+  });
+});
