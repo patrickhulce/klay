bd81ff834153627913602d4ccb6cc619311bcf5f
Patrick Hulce <phulce@google.com>
2017-03-11 22:35:47 -0800

    test: add more QueryBuilder tests

--- packages/klay-kiln-sql/test/QueryBuilder.test.js
+++ packages/klay-kiln-sql/test/QueryBuilder.test.js
@@ -1,7 +1,40 @@
 defineTest('QueryBuilder.js', function (QueryBuilder) {
   describe('#toObject', function () {
     it('should return the query', function () {
-      new QueryBuilder(null, {}).toObject().should.eql({});
+      const query = {foo: 'bar'}
+      const result = new QueryBuilder(null, null, query).toObject()
+      result.should.eql(query)
+      result.should.not.equal(query)
     });
   });
+
+  describe('#where', function () {
+    it('should set objects directly', function () {
+      const result = new QueryBuilder(null, null)
+        .where({myfield: {$lte: 15}})
+        .toObject()
+      result.should.eql({where: {myfield: {$lte: 15}}})
+    })
+  })
+
+  describe('#fetchCount', function () {
+    it('should fail when used without a model', function () {
+      const func = () => new QueryBuilder(null, null).fetchCount()
+      (func).should.throw(/no model/)
+    })
+  })
+
+  describe('fetchResult', function () {
+    it('should fail when used without a model', function () {
+      const func = () => new QueryBuilder(null, null).fetchCount()
+      (func).should.throw(/no model/)
+    })
+  })
+
+  describe('fetchResults', function () {
+    it('should fail when used without a model', function () {
+      const func = () => new QueryBuilder(null, null).fetchCount()
+      (func).should.throw(/no model/)
+    })
+  })
 });
