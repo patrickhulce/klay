253d858c60250931b8b640c94833ad1dab103535
Patrick Hulce <patrick.hulce@gmail.com>
2018-01-20 23:07:48 -0800

    feat(model): add format

--- lib-ts/typedefs.ts
+++ lib-ts/typedefs.ts
@@ -1,11 +1,14 @@
 export interface IModel {
   type(type: string): IModel
+  format(format: string): IModel
 }
 
 export interface IModelSpecification {
-  type?: string
+  type?: string,
+  format?: string,
 }
 
 export interface IModelOptions {
-  types: string[]
+  types: string[],
+  formats: {[typeName: string]: string[]},
 }
