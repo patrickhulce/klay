a0497cbe02d5aafae4186c466fd898818179012c
Patrick Hulce <patrick.hulce@gmail.com>
2018-01-20 23:07:48 -0800

    feat(model): add format

--- packages/klay/test/ts-model.test.js
+++ packages/klay/test/ts-model.test.js
@@ -2,6 +2,11 @@ const expect = require('chai').expect
 const Model = require('../lib-ts/model').Model
 
 describe('model.ts', () => {
+  const defaultOptions = {
+    types: ['string'],
+    formats: {string: ['lowercase']},
+  }
+
   describe('#constructor', () => {
     it('should construct a model', () => {
       expect(() => new Model({}, {types: []})).to.not.throw()
@@ -10,12 +15,27 @@ describe('model.ts', () => {
 
   describe('.type', () => {
     it('should set spec.type', () => {
-      const model = new Model({}, {types: ['string']}).type('string')
+      const model = new Model({}, defaultOptions).type('string')
       expect(model.spec.type).to.equal('string')
     })
 
     it('should throw on unacceptable type', () => {
-      expect(() => new Model({}, {types: []}).type('string')).to.throw()
+      expect(() => new Model({}, defaultOptions).type('unknown')).to.throw()
+    })
+  })
+
+  describe('.format', () => {
+    it('should set spec.format', () => {
+      const model = new Model({}, defaultOptions).type('string').format('lowercase')
+      expect(model.spec.format).to.equal('lowercase')
+    })
+
+    it('should throw on format without type', () => {
+      expect(() => new Model({}, defaultOptions).format('string')).to.throw(/type must be set/)
+    })
+
+    it('should throw on unacceptable format', () => {
+      expect(() => new Model({}, defaultOptions).type('string').format('unknown')).to.throw()
     })
   })
 })
