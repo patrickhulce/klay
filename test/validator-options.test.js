b0b5b782605f10747fd8710293dc7f56bd1dfe88
Patrick Hulce <patrick.hulce@gmail.com>
2018-01-27 00:17:44 -0800

    feat(validator-options): add proper clone support

--- test/validator-options.test.js
+++ test/validator-options.test.js
@@ -6,17 +6,23 @@ const emptyArr = {___ALL_FORMATS___: [], ___NO_FORMAT___: []}
 describe('lib/validator-options.ts', () => {
   describe('#constructor', () => {
     it('creates successfully', () => {
-      const options = new Options({types: ['string']})
+      const types = ['string']
+      const options = new Options({types})
       expect(options.types).to.eql(['string'])
+      expect(options.types).to.not.equal(types)
       expect(options.formats).to.eql({string: []})
       expect(options.coerce).to.eql({string: emptyObj})
       expect(options.validations).to.eql({string: emptyArr})
     })
 
     it('fills in missing formats', () => {
-      const options = new Options({types: ['string'], formats: {string: ['name']}})
+      const types = ['string']
+      const formats = {string: ['name']}
+      const options = new Options({types, formats})
       expect(options.types).to.eql(['string'])
+      expect(options.types).to.not.equal(types)
       expect(options.formats).to.eql({string: ['name']})
+      expect(options.formats).to.not.equal(formats)
       expect(options.coerce).to.eql({string: Object.assign({}, emptyObj, {name: {}})})
       expect(options.validations).to.eql({string: Object.assign({}, emptyArr, {name: []})})
     })
@@ -48,10 +54,37 @@ describe('lib/validator-options.ts', () => {
     })
   })
 
-  describe('#sanitize', () => {
+  describe('.clone', () => {
+    it('deep clones the options', () => {
+      const original = {types: ['string']}
+      const optionsA = new Options(original)
+      const optionsB = optionsA.clone()
+      optionsA.types.push('a')
+      optionsB.types.push('b')
+      expect(original.types).to.eql(['string'])
+      expect(optionsA.types).to.eql(['string', 'a'])
+      expect(optionsB.types).to.eql(['string', 'b'])
+    })
+
+    it('deep clones the nested objects', () => {
+      const parseFn = () => ''
+      const allFormatsCoerce = {parse: parseFn}
+      const coerce = {string: {___ALL_FORMATS___: allFormatsCoerce}}
+      const original = {types: ['string'], coerce}
+      const optionsA = new Options(original)
+      const optionsB = optionsA.clone()
+      optionsA.coerce.string.___ALL_FORMATS___.foo = 1
+      optionsB.coerce.string.___ALL_FORMATS___.bar = 2
+      expect(original.coerce.string.___ALL_FORMATS___).to.eql({parse: parseFn})
+      expect(optionsA.coerce.string.___ALL_FORMATS___).to.eql({parse: parseFn, foo: 1})
+      expect(optionsB.coerce.string.___ALL_FORMATS___).to.eql({parse: parseFn, bar: 2})
+    })
+  })
+
+  describe('#from', () => {
     it('fills in missing properties', () => {
       const input = {types: ['string']}
-      const options = Options.sanitize(input)
+      const options = Options.from(input)
       expect(options).to.not.equal(input)
       expect(options.types).to.eql(['string'])
       expect(options.formats).to.eql({string: []})
@@ -61,8 +94,31 @@ describe('lib/validator-options.ts', () => {
 
     it('reuses existing options', () => {
       const input = new Options({types: ['string']})
-      const options = Options.sanitize(input)
+      const options = Options.from(input)
       expect(options).to.equal(input)
+    })
+  })
+
+  describe('#merge', () => {
+    it('merges two options without touching original', () => {
+      const inputA = {types: ['string']}
+      const inputB = {types: ['number']}
+      const options = Options.merge(inputA, inputB)
+      expect(options).to.eql({
+        types: ['string', 'number'],
+        formats: {string: [], number: []},
+        coerce: {
+          string: emptyObj,
+          number: emptyObj,
+        },
+        validations: {
+          string: emptyArr,
+          number: emptyArr,
+        },
+      })
+
+      expect(inputA).to.eql({types: ['string']})
+      expect(inputB).to.eql({types: ['number']})
     })
   })
 })
