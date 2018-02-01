7e9cef6f7aa41bf42614bd9305e610ba6469befe
Patrick Hulce <patrick.hulce@gmail.com>
2018-01-31 21:27:49 -0800

    feat(extensions): add default type-coerce

---
+++ lib-ts/extensions/defaults/type-coerce.ts
@@ -0,0 +1,81 @@
+import {assertions} from '../../errors/validation-error'
+import {ALL_FORMATS, IValidatorCoerce, ValidationPhase} from '../../typedefs'
+
+export const coerce: IValidatorCoerce = {
+  boolean: {
+    [ALL_FORMATS]: {
+      [ValidationPhase.TypeCoerce]: (validationResult, spec) => {
+        let value = validationResult.value
+        if (!spec.strict) {
+          if (value === 'true') {
+            value = true
+          } else if (value === 'false') {
+            value = false
+          }
+        }
+
+        assertions.typeof(value, 'boolean')
+        return validationResult.setValue(value)
+      },
+    },
+  },
+  number: {
+    [ALL_FORMATS]: {
+      [ValidationPhase.TypeCoerce]: (validationResult, spec) => {
+        let value = validationResult.value
+        if (!spec.strict && typeof value === 'string') {
+          const cast = Number(value)
+          if (value && !Number.isNaN(cast)) {
+            value = cast
+          }
+        }
+
+        assertions.typeof(value, 'number')
+        return validationResult.setValue(value)
+      },
+    },
+  },
+  string: {
+    [ALL_FORMATS]: {
+      [ValidationPhase.TypeCoerce]: (validationResult, spec) => {
+        let value = validationResult.value
+        if (!spec.strict && typeof value !== 'object' && typeof value !== 'undefined') {
+          value = String(value)
+        }
+
+        assertions.typeof(value, 'string')
+        return validationResult.setValue(value)
+      },
+    },
+  },
+  object: {
+    [ALL_FORMATS]: {
+      [ValidationPhase.TypeCoerce]: (validationResult, spec) => {
+        let value = validationResult.value
+        if (!spec.strict && typeof value === 'string') {
+          try {
+            value = JSON.parse(value)
+          } catch (e) {}
+        }
+
+        assertions.typeof(value, 'object')
+        return validationResult.setValue(value)
+      },
+    },
+  },
+  array: {
+    [ALL_FORMATS]: {
+      [ValidationPhase.TypeCoerce]: (validationResult, spec) => {
+        let value = validationResult.value
+        if (!spec.strict && typeof value === 'string') {
+          try {
+            value = JSON.parse(value)
+          } catch (e) {}
+        }
+
+        assertions.typeof(value, 'array')
+        return validationResult.setValue(value)
+      },
+    },
+  },
+}
