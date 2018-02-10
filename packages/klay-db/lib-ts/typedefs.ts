04f9c66aafa858e5ba17a59e7ec0f4a8bef328df
Patrick Hulce <patrick.hulce@gmail.com>
2018-02-10 14:05:27 -0800

    chore(build): initial typescript configuration

---
+++ packages/klay-db/lib-ts/typedefs.ts
@@ -0,0 +1,84 @@
+import {ValidationPhase} from 'klay'
+
+export interface IDatabaseOptions {
+  spec: IDatabaseSpecification
+
+  automanage(property: IAutomanageProperty): IDatabaseOptions
+  constraint(property: IConstraint): IDatabaseOptions
+  index(property: IIndexPropertyInput[]): IDatabaseOptions
+  reset(): IDatabaseOptions
+}
+
+export interface IDatabaseSpecification {
+  automanage?: IAutomanageProperty[]
+  constraint?: IConstraint[]
+  index?: IIndexProperty[][]
+}
+
+export interface IAutomanageProperty {
+  property: PropertyPath
+  event: DatabaseEvent
+  phase: DatabasePhase
+  supplyWith: ISupplyWithFunction | SupplyWithPreset
+}
+
+export interface IConstraint {
+  name?: string
+  properties: PropertyPath[]
+  type: ConstraintType
+  meta: IConstraintMeta
+}
+
+export interface IConstraintMeta {
+  name?: string
+  behavior?: ConstraintBehavior
+  lookupTable?: string
+}
+
+export interface IIndexProperty {
+  property: PropertyPath
+  direction: IndexDirection
+}
+
+export type IIndexPropertyInput = PropertyPath | IIndexProperty
+
+export enum DatabaseEvent {
+  All = '*',
+  Create = 'create',
+  Update = 'update',
+}
+
+export enum DatabasePhase {
+  PreValidate = 'pre-validate',
+  PostValidate = 'post-validate',
+  DelegateToDatabase = 'delegate',
+}
+
+export type PropertyPath = string[]
+
+export type ISupplyWithFunction = (original: any) => any
+
+export enum SupplyWithPreset {
+  Autoincrement = 'autoincrement',
+  Date = 'date',
+  ISOTimestamp = 'iso-timestamp',
+  UUID = 'uuid',
+}
+
+export enum ConstraintType {
+  Primary = 'primary',
+  Unique = 'unique',
+  Reference = 'reference',
+  Immutable = 'immutable',
+  Custom = 'custom',
+}
+
+export enum ConstraintBehavior {
+  Reject = 'reject',
+  Update = 'update',
+}
+
+export enum IndexDirection {
+  Ascending = 'asc',
+  Descending = 'desc',
+}
