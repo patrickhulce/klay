fdbdb5abd167b1dc468a076cf307a0c5b2d81707
Patrick Hulce <patrick.hulce@gmail.com>
2018-02-10 14:17:48 -0800

    feat(options): create DatabaseOptions class

---
+++ packages/klay-db/lib-ts/options.ts
@@ -0,0 +1,102 @@
+import {assertions, ModelError} from 'klay/lib/errors/model-error'
+import {
+  IDatabaseSpecification,
+  IDatabaseOptions,
+  IAutomanageProperty,
+  IConstraint,
+  PropertyPath,
+  DatabaseEvent,
+  DatabasePhase,
+  SupplyWithPreset,
+  ISupplyWithFunction,
+  ConstraintType,
+  IIndexProperty,
+  IndexDirection,
+  IIndexPropertyInput,
+} from './typedefs'
+import {values} from 'lodash'
+import {v4 as uuid} from 'uuid'
+
+export class DatabaseOptions implements IDatabaseOptions {
+  public spec: IDatabaseSpecification
+
+  public constructor(spec?: IDatabaseSpecification) {
+    this.spec = spec || {}
+  }
+
+  public automanage(property: IAutomanageProperty): IDatabaseOptions {
+    assertions.typeof(property, 'object', 'automanage')
+    property.supplyWith = DatabaseOptions._determineSupplyWith(property.supplyWith)
+    assertions.typeof(property.property, 'array', 'automanage.propertyPath')
+    assertions.oneOf(property.event, values(DatabaseEvent), 'automanage.event')
+    assertions.oneOf(property.phase, values(DatabasePhase), 'automanage.phase')
+
+
+    const automanage = this.spec.automanage || []
+    automanage.push(property)
+    this.spec.automanage = automanage
+    return this
+  }
+
+  public constraint(constraint: IConstraint): IDatabaseOptions {
+    assertions.typeof(constraint, 'object', 'constraint')
+    constraint.meta = constraint.meta || {}
+    assertions.typeof(constraint.properties, 'array', 'constraint.propertyPaths')
+    assertions.oneOf(constraint.type, values(ConstraintType), 'constraint.type')
+    assertions.typeof(constraint.meta, 'object')
+    const propertiesAsString = constraint.properties.map(prop => prop.join('.')).join(',')
+    constraint.name = constraint.name || `${constraint.type}:${propertiesAsString}`
+
+    const constraints = this.spec.constraint || []
+    constraints.push(constraint)
+    this.spec.constraint = constraints
+    return this
+  }
+
+  public index(properties: IIndexPropertyInput[]): IDatabaseOptions {
+    assertions.typeof(properties, 'array', 'index')
+    properties.forEach((item, i) => {
+      properties[i] = DatabaseOptions._determineIndex(item, i)
+    })
+
+    const indexes = this.spec.index || []
+    indexes.push(properties as IIndexProperty[])
+    this.spec.index = indexes
+    return this
+  }
+
+  public reset(): IDatabaseOptions {
+    this.spec = {}
+    return this
+  }
+
+  private static _determineSupplyWith(
+    supplyWith: ISupplyWithFunction | SupplyWithPreset
+  ): ISupplyWithFunction | SupplyWithPreset {
+    if (supplyWith === SupplyWithPreset.Autoincrement || typeof supplyWith === 'function') {
+      return supplyWith
+    }
+
+    switch (supplyWith) {
+      case SupplyWithPreset.Date:
+        return () => new Date()
+      case SupplyWithPreset.ISOTimestamp:
+        return () => new Date().toISOString()
+      case SupplyWithPreset.UUID:
+        return () => uuid()
+      default:
+        throw new ModelError('invalid automanage supplyWith')
+    }
+  }
+
+  private static _determineIndex(property: IIndexPropertyInput, i: number): IIndexProperty {
+    if (Array.isArray(property)) {
+      return {property, direction: IndexDirection.Ascending}
+    }
+
+    assertions.typeof(property, 'object')
+    assertions.typeof(property.property, 'array', `index.${i}.property`)
+    assertions.oneOf(property.direction, values(IndexDirection), `index.${i}.property`)
+    return property
+  }
+}
---
+++ packages/klay-db/test/ts-options.test.js
@@ -0,0 +1,283 @@
+const expect = require('chai').expect
+const Options = require('../lib-ts/options').DatabaseOptions
+
+describe.only('lib/options.ts', () => {
+  describe('#constructor', () => {
+    it('should set spec properties', () => {
+      const opts = new Options({index: [{property: 'foo', direction: 'asc'}]})
+      expect(opts).to.be.instanceof(Options)
+      expect(opts).to.have.nested.property('spec.index.0.property', 'foo')
+    })
+  })
+
+  describe('.automanage', () => {
+    let opts
+
+    beforeEach(() => {
+      opts = new Options()
+    })
+
+    it('should add the first automanaged property', () => {
+      const supplyWith = () => new Date()
+      opts = opts.automanage({
+        property: ['myprop'],
+        event: 'create',
+        phase: 'post-validate',
+        supplyWith,
+      })
+
+      expect(opts.spec)
+        .to.have.property('automanage')
+        .eql([
+          {
+            property: ['myprop'],
+            event: 'create',
+            phase: 'post-validate',
+            supplyWith,
+          },
+        ])
+    })
+
+    it('should add the second automanaged property', () => {
+      const supplyWith = () => new Date()
+      opts = opts
+        .automanage({
+          property: ['mypropA'],
+          event: 'create',
+          phase: 'post-validate',
+          supplyWith: () => 1,
+        })
+        .automanage({
+          property: ['mypropB'],
+          event: 'update',
+          phase: 'pre-validate',
+          supplyWith,
+        })
+
+      expect(opts.spec)
+        .to.have.nested.property('automanage.1')
+        .eql({
+          property: ['mypropB'],
+          event: 'update',
+          phase: 'pre-validate',
+          supplyWith,
+        })
+    })
+
+    it('should support autoincrement supplyWith', () => {
+      opts = opts.automanage({
+        property: ['mypropA'],
+        event: 'create',
+        phase: 'delegate',
+        supplyWith: 'autoincrement',
+      })
+
+      expect(opts.spec)
+        .to.have.nested.property('automanage.0')
+        .eql({
+          property: ['mypropA'],
+          event: 'create',
+          phase: 'delegate',
+          supplyWith: 'autoincrement',
+        })
+    })
+
+    it('should support date supplyWith', () => {
+      opts = opts.automanage({
+        property: ['mypropA'],
+        event: 'create',
+        phase: 'delegate',
+        supplyWith: 'date',
+      })
+
+      expect(opts.spec)
+        .to.have.nested.property('automanage.0.supplyWith')
+        .a('function')
+      opts.spec.automanage[0].supplyWith().should.be.an.instanceof(Date)
+    })
+
+    it('should support isotimestamp supplyWith', () => {
+      opts = opts.automanage({
+        property: ['mypropA'],
+        event: 'create',
+        phase: 'delegate',
+        supplyWith: 'iso-timestamp',
+      })
+
+      expect(opts.spec)
+        .to.have.nested.property('automanage.0.supplyWith')
+        .a('function')
+      expect(opts.spec.automanage[0].supplyWith()).to.match(/^\d{4}-\d{2}-\d{2}/)
+    })
+
+    it('should fail when given invalid input', () => {
+      const valid = {property: [], event: '*', phase: 'delegate', supplyWith: () => 1}
+      expect(() => opts.automanage('old')).to.throw()
+      expect(() => opts.automanage({...valid, property: 'foo'})).to.throw(/property/)
+      expect(() => opts.automanage({...valid, event: ''})).to.throw(/event/)
+      expect(() => opts.automanage({...valid, phase: ''})).to.throw(/phase/)
+      expect(() => opts.automanage({...valid, supplyWith: ''})).to.throw(/supplyWith/)
+    })
+  })
+
+  describe.only('.constraint', () => {
+    let opts
+
+    beforeEach(() => {
+      opts = new Options()
+    })
+
+    it('should add the first constraint property', () => {
+      opts = opts.constraint({properties: [['id']], type: 'primary'})
+
+      expect(opts.spec)
+        .to.have.property('constraint')
+        .eql([
+          {
+            name: 'primary:id',
+            properties: [['id']],
+            type: 'primary',
+            meta: {},
+          },
+        ])
+    })
+
+    it('should add the second constraint property', () => {
+      opts = opts
+        .constraint({properties: [['id']], type: 'primary'})
+        .constraint({properties: [['day'], ['other']], type: 'unique', meta: {behavior: 'reject'}})
+
+      expect(opts.spec)
+        .to.have.nested.property('constraint.1')
+        .eql({
+          name: 'unique:day,other',
+          properties: [['day'], ['other']],
+          type: 'unique',
+          meta: {behavior: 'reject'},
+        })
+    })
+
+    it('should add a reference constraint property', () => {
+      const meta = {lookupTable: 'parents'}
+      opts = opts.constraint({properties: [['id']], type: 'primary'}).constraint({
+        name: 'reference:parent',
+        properties: [['parent_id']],
+        type: 'reference',
+        meta,
+      })
+
+      expect(opts.spec)
+        .to.have.nested.property('constraint.1')
+        .eql({
+          name: 'reference:parent',
+          properties: [['parent_id']],
+          type: 'reference',
+          meta,
+        })
+    })
+
+    it('should add an immutable constraint property', () => {
+      opts = opts
+        .constraint({properties: [['id']], type: 'primary'})
+        .constraint({properties: [['canonical_id'], ['created_on']], type: 'immutable'})
+
+      expect(opts.spec)
+        .to.have.nested.property('constraint.1')
+        .eql({
+          name: 'immutable:canonical_id,created_on',
+          properties: [['canonical_id'], ['created_on']],
+          type: 'immutable',
+          meta: {},
+        })
+    })
+
+    it('should add a custom constraint property', () => {
+      opts = opts.constraint({
+        properties: [['something'], ['other']],
+        type: 'custom',
+        meta: {foo: 'bar'},
+      })
+
+      expect(opts.spec)
+        .to.have.nested.property('constraint.0')
+        .eql({
+          name: 'custom:something,other',
+          properties: [['something'], ['other']],
+          type: 'custom',
+          meta: {foo: 'bar'},
+        })
+    })
+  })
+
+  describe.only('.index', () => {
+    let opts
+
+    beforeEach(() => {
+      opts = new Options()
+    })
+
+    it('should add the first index', () => {
+      opts = opts.index([{property: ['myprop'], direction: 'desc'}])
+      expect(opts.spec)
+        .to.have.property('index')
+        .eql([[{property: ['myprop'], direction: 'desc'}]])
+    })
+
+    it('should add the second index', () => {
+      opts = opts.index([['myprop']]).index([['otherprop'], ['second']])
+
+      expect(opts.spec)
+        .to.have.nested.property('index.1')
+        .eql([
+          {property: ['otherprop'], direction: 'asc'},
+          {property: ['second'], direction: 'asc'},
+        ])
+    })
+
+    it('should add a mixed index', () => {
+      opts = opts.index([['some'], {property: ['foo'], direction: 'desc'}])
+      expect(opts.spec)
+        .to.have.nested.property('index.0')
+        .eql([{property: ['some'], direction: 'asc'}, {property: ['foo'], direction: 'desc'}])
+    })
+  })
+
+  describe('.toObject', () => {
+    const now = () => new Date()
+    const checksum = item => item.id * Math.random()
+
+    it('should return all of the settings', () => {
+      const meta = {lookupTable: 'parents', name: 'reference:parent'}
+      const opts = new Options()
+        .index('type')
+        .index(['type', 'size', '-created_at'])
+        .automanage('checksum', 'update', 'post-validate', checksum)
+        .automanage('created_at', 'create', now)
+        .automanage('updated_at', 'update', now)
+        .constraint('id', 'primary')
+        .constraint(['type', 'version'], 'unique')
+        .constraint(['parent_id'], 'reference', meta)
+
+      expect(opts.spec).to.eql({
+        indexes: [
+          [{property: ['type'], direction: 'asc'}],
+          [
+            {property: ['type'], direction: 'asc'},
+            {property: ['size'], direction: 'asc'},
+            {property: ['created_at'], direction: 'desc'},
+          ],
+        ],
+        automanage: [
+          {property: ['checksum'], event: 'update', phase: 'post-validate', supplyWith: checksum},
+          {property: ['created_at'], event: 'create', phase: 'pre-validate', supplyWith: now},
+          {property: ['updated_at'], event: 'update', phase: 'pre-validate', supplyWith: now},
+        ],
+        constraints: [
+          {name: 'primary:id', properties: ['id'], type: 'primary', meta: {}},
+          {name: 'unique:type,version', properties: ['type', 'version'], type: 'unique', meta: {}},
+          {name: 'reference:parent', properties: ['parent_id'], type: 'reference', meta},
+        ],
+      })
+    })
+  })
+})
