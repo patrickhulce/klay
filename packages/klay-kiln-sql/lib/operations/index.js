2d561dc511c4d109df97304d62f8c2a9452f42d5
Patrick Hulce <patrick.hulce@gmail.com>
2018-03-04 08:51:49 -0800

    refactor(sql): delete old js

--- packages/klay-kiln-sql/lib/operations/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  create: require('./create'),
-  update: require('./update'),
-  upsert: require('./upsert'),
-  patch: require('./patch'),
-}
--- packages/klay-kiln-sql/lib/shared/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.assign(
-  {},
-  require('./constraints'),
-  require('./primaryKey'),
-  require('./serialization')
-)
