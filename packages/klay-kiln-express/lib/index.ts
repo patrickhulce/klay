4c4df61d93ff14ea49134736c71311ce930d690f
Patrick Hulce <patrick.hulce@gmail.com>
2018-03-12 20:33:04 -0700

    feat(express): add default export

---
+++ packages/klay-kiln-express/lib/index.ts
@@ -0,0 +1,5 @@
+export {RouteExtension} from './extensions/route'
+export {RouterExtension} from './extensions/router'
+export {createValidationMiddleware} from './helpers/create-middleware'
+export {createRoute} from './helpers/create-route'
+export * from './typedefs'
