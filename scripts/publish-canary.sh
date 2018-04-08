4bf310483028e339ac3cc94870d1a907c9fa1d71
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-07 19:58:06 -0700

    chore(travis): fix publish canary script

---
+++ scripts/publish-canary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$NPM_TOKEN" ] && [ ! -e ~/.npmrc ]; then
+  echo 'No NPM Token set!'
+  exit 1
+fi
+
+if [ ! -e ~/.npmrc ]; then
+  cat > ~/.npmrc <<EOF
+username=patrickhulce
+email=patrick.hulce@gmail.com
+author=Patrick Hulce <patrick.hulce@gmail.com>
+//registry.npmjs.org/:_authToken=$NPM_TOKEN
+EOF
+fi
+
+lerna publish --canary --npm-tag=next --yes
