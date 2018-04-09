d7ca88f183063c13a3702226f26011afbad494b8
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-09 10:24:45 -0700

    chore(travis): use echo for npmrc over cat

--- scripts/publish-canary.sh
+++ scripts/publish-canary.sh
@@ -6,14 +6,15 @@ if [ -z "$NPM_TOKEN" ] && [ ! -e ~/.npmrc ]; then
 fi
 
 if [ ! -e ~/.npmrc ]; then
-  cat > ~/.npmrc <<EOF
-username=patrickhulce
-email=patrick.hulce@gmail.com
-author=Patrick Hulce <patrick.hulce@gmail.com>
-//registry.npmjs.org/:_authToken=$NPM_TOKEN
-EOF
+  echo "Creating .npmrc file..."
+
+  echo "//registry.npmjs.org/:username=patrickhulce" > ~/.npmrc
+  echo "//registry.npmjs.org/:email=patrick.hulce@gmail.com" >> ~/.npmrc
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 fi
 
-npm whoami
+head -n 2 ~/.npmrc
+
+npm whoami || exit 1
 npm access ls-packages
-lerna publish --canary --npm-tag=next --yes || exit 1
+lerna publish --canary --npm-tag=next --yes
