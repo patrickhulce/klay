6dbb5a0aca5ece3b825abbc94046c5fcfedbd0e6
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-09 10:39:13 -0700

    chore(travis): prefer NPM_TOKEN over npmrc

--- scripts/publish-canary.sh
+++ scripts/publish-canary.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-if [ -z "$NPM_TOKEN" ] && [ ! -e ~/.npmrc ]; then
+# Check for token existence without leaking it to stdout
+grep Token ~/.npmrc > /dev/null || grep Token .npmrc > /dev/null
+
+if [ -n "$NPM_TOKEN" ]; then
+  echo "Creating .npmrc file in local directory..."
+
+  echo "//registry.npmjs.org/:username=patrickhulce" > .npmrc
+  echo "//registry.npmjs.org/:email=patrick.hulce@gmail.com" >> .npmrc
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
+elif [ $? != 0 ]; then
   echo 'No NPM Token set!'
   exit 1
 fi
-
-if [ ! -e ~/.npmrc ]; then
-  echo "Creating .npmrc file..."
-
-  echo "//registry.npmjs.org/:username=patrickhulce" > ~/.npmrc
-  echo "//registry.npmjs.org/:email=patrick.hulce@gmail.com" >> ~/.npmrc
-  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-fi
-
-head -n 2 ~/.npmrc
 
 npm whoami || exit 1
 npm access ls-packages
