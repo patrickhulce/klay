e39a41629f4a9e8abc995f77b514aee624e651ee
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-16 20:06:56 -0700

    chore(scripts): custom canary version

--- scripts/publish-canary.sh
+++ scripts/publish-canary.sh
@@ -14,5 +14,17 @@ elif [ $? != 0 ]; then
   exit 1
 fi
 
+# Double-check that we're logged in to npm
 npm whoami || exit 1
-lerna publish --canary --npm-tag=next --yes
+
+# Get the last real release
+LAST_GIT_TAG=$(git describe --abbrev=0 --tags)
+TIMESTAMP_OF_LAST_TAG=$(git show -s --format="%ct" $LAST_GIT_TAG)
+UNIX_TIMESTAMP=$(date +%s)
+TIMESTAMP_DIFF=$(($UNIX_TIMESTAMP-$TIMESTAMP_OF_LAST_TAG))
+BUILD_VERSION=$((TIMESTAMP_DIFF/60))
+
+NEXT_NPM_PREMINOR=$(npx semver -i preminor --preid alpha $LAST_GIT_TAG)
+NEXT_VERSION=$(echo $NEXT_NPM_PREMINOR | sed s/alpha.0/alpha.$BUILD_VERSION/)
+# TODO: remove the --exact flag when moving back ^ dependencies
+lerna publish --exact --repo-version "$NEXT_VERSION" --npm-tag=next --yes
